4d8927f007de9c3616a794dcf43ae31f64a08b78
Bastian laptop WSL <bastian.kristensen@outlook.com>
2025-09-05 12:57:21 +0200

    error in the __all__ attribute

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,12 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .bloch_plotly import plot_bloch_plotly
-from .prettyprint_qutip import prettyprint, format_complex, _add_parenthesis
-
-# Expose aliases
-plot_bloch = plot_bloch_plotly
-pprint = prettyprint
+from .bloch_plotly import plot_bloch_plotly as plot_bloch
+from .prettyprint_qutip import prettyprint, format_complex
+from .prettyprint_qutip import prettyprint as pprint
 
 __all__ = ["prettyprint", "pprint", 
            "format_complex", 
-           "plot_bloch_plotly", "Bloch"
+           "plot_bloch_plotly", "plot_bloch",
+           "__version__"
     ]
